8ceac8b7396b36762bcaeaa1e8d38854095fd3bb
xoliio <xoliio@gmail.com>
2020-11-07 13:25:29 -0800

    adds static controller, images, and welcome view page

--- magic-tutoring/config/routes.rb
+++ magic-tutoring/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :conclaves
   resources :subjects
   resources :apprentices
+  get '/', to: 'static#welcome', as: 'welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
