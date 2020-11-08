250a4f2c470f4fabfd1fe309f911bcf86dc61c06
wlytle <wlytle89@gmail.com>
2020-11-08 16:28:06 -0700

    adds new professor page

--- magic-tutoring/config/routes.rb
+++ magic-tutoring/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :apprentices
   resources :sessions, only: [:new, :create, :destroy]
   get "/", to: "static#welcome", as: "welcome"
-  get "/inter", to: "static#inter", as: "intermediate"
+  get "/intermediate", to: "static#intermediate", as: "intermediate"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
