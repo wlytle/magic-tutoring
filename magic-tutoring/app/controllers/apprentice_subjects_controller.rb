ea325ad39fed6c242c64ab8bda8c068a7b4da2b1
xoliio <xoliio@gmail.com>
2020-11-09 10:15:03 -0800

    button to add a subject for student on subject show page

--- magic-tutoring/app/controllers/apprentice_subjects_controller.rb
+++ magic-tutoring/app/controllers/apprentice_subjects_controller.rb
@@ -1,6 +1,19 @@
 class ApprenticeSubjectsController < ApplicationController
+  
+  def create
+    @apprentice = current_user
+    @subject = Subject.find(params[:format])
+    @appsub = ApprenticeSubject.new(subject: @subject, apprentice: @apprentice)
+      if @appsub.save
+        redirect_to apprentice_path(@apprentice)
+      else
+        redirect_to @subject
+      end
+  end
+  
   def delete
     # delete the apprenticeSubsject
     redirect_to # back to the apprentice
   end
+
 end
