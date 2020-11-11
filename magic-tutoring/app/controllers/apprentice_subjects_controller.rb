ef63864ea20fab51e686f3690aa706583c7bf1a8
mandareis <amanda@depaula.xyz>
2020-11-11 10:16:27 -0800

    adds subject delete button and subjects add button to apprentice page

--- magic-tutoring/app/controllers/apprentice_subjects_controller.rb
+++ magic-tutoring/app/controllers/apprentice_subjects_controller.rb
@@ -1,19 +1,19 @@
 class ApprenticeSubjectsController < ApplicationController
-  
   def create
     @apprentice = current_user
     @subject = Subject.find(params[:format])
     @appsub = ApprenticeSubject.new(subject: @subject, apprentice: @apprentice)
-      if @appsub.save
-        redirect_to apprentice_path(@apprentice)
-      else
-        redirect_to @subject
-      end
-  end
-  
-  def delete
-    # delete the ApprenticeSubject
-    redirect_to # back to the apprentice
+    if @appsub.save
+      redirect_to apprentice_path(@apprentice)
+    else
+      redirect_to @subject
+    end
   end
 
+  def destroy
+    @as = ApprenticeSubject.find(params[:id])
+    @as.destroy
+    @apprentice = current_user
+    redirect_to apprentice_path(@apprentice)
+  end
 end
--- magic-tutoring/config/routes.rb
+++ magic-tutoring/config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "/about", to: "static#about", as: "about"
   get "/intermediate", to: "static#intermediate", as: "intermediate"
   get "/subject/:id/conclave", to: "conclaves#new", as: "new_conclave"
+
   #post "/apprentice/:id/apprentice_subjects", to: "apprentice_subjects#create", as: "make_apprentice_subject"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
